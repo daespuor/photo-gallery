01ce5e2aec80e46231f5151f3b708bb3c10dcaf1
Daniel <daespuor1991@gmail.com>
2024-04-01 21:21:58 -0500

    Add a middleware for CORS

---
+++ README.md
@@ -0,0 +1,8 @@
+# Next steps
+
+- Add a CORS middleware
+- Add variable bitrate
+- Modify the Tab 3 to add more than one video
+- Add login to see videos
+    - JWT
+    - MFA
--- server/main.go
+++ server/main.go
@@ -7,6 +7,15 @@ import (
 	"strings"
 )
 
+type NextFunc func(w http.ResponseWriter, req *http.Request)
+
+func AddCORS(nextFunc NextFunc) NextFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		nextFunc(w, req)
+	}
+}
+
 func handlePlaylist(w http.ResponseWriter, req *http.Request) {
 	filepath, errWD := os.Getwd()
 	if errWD != nil {
@@ -17,16 +26,15 @@ func handlePlaylist(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		http.Error(w, "Error reading the playlist.m3u8", http.StatusInternalServerError)
 	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/vnd.apple.mpegurl")
 	w.Write(playlist)
 }
 func handleStaticContent(prefix string, root http.FileSystem) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+	handler := func(w http.ResponseWriter, r *http.Request) {
 		r.URL.Path = strings.TrimPrefix(r.URL.Path, prefix)
 		http.FileServer(root).ServeHTTP(w, r)
-	})
+	}
+	return http.HandlerFunc(AddCORS(handler))
 }
 func main() {
 	currentPath, errWD := os.Getwd()
@@ -37,7 +45,7 @@ func main() {
 	currentPath += videoPrefix
 
 	server := http.NewServeMux()
-	server.HandleFunc("/playlist.m3u8", handlePlaylist)
+	server.HandleFunc("/playlist.m3u8", AddCORS(handlePlaylist))
 	server.Handle("/", handleStaticContent(videoPrefix, http.Dir(currentPath)))
 	http.ListenAndServe(":3033", server)
 	fmt.Println("vim-go")
